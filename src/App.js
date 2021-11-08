004605343f8ea6fbf146b9888960a8ef234721c7
Shikha Gupta <shikha981492@gmail.com>
2021-11-08 07:42:37 +0530

    TextUtils is an application which allows user to manipulate text according to his/her needs.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import About from "./components/About";
+import Alert from "./components/Alert";
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (msg, type) => {
+    setAlert({
+      msg: msg,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "rgb(67 83 98)";
+      // document.title = "TextUtils - Dark mode";
+      showAlert("Dark mode enabled", "success");
+      // setInterval(() => {
+      //   document.title = "TextUtils is amazing!!";
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = "Install TextUtils!!";
+      // }, 1500)
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      // document.title = "TextUtils - Light mode";
+      showAlert("Light mode enabled", "success");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //Code below is JSX -> just like HTML code but the reserved keywords of JS such as class can't be used as such
+    <>
+      {/* <BrowserRouter> */}
+        {/*fragment wrapper -> cannot add tags seperately, either group them into div or use fragment wrapper*/}
+        <Navbar title="MyApp" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        {/* <Navbar /> */}
+          {/* <Routes>
+            <Route
+              exact path="/"
+              element={
+                <TextForm
+                  heading="Enter the text below"
+                  mode={mode}
+                  showAlert={showAlert}
+                />
+              }
+            /> */}
+            {/* <Route exact path="/About" element={<About mode={mode} />} />
+          </Routes> */}
+        <TextForm heading="Enter the text below" mode={mode} showAlert={showAlert}/>
+        {/* <About mode={mode} /> */}
+      {/* </BrowserRouter> */}
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        // console.log("Uppercase was clicked");
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to uppercase", "success");
+    }
+
+    const handleLowClick = () => {
+        // console.log("Uppercase was clicked");
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to lowercase", "success");
+    }
+
+    const handleClearClick = () => {
+        // console.log("Uppercase was clicked");
+        let newText = "";
+        setText(newText);
+        props.showAlert("Cleared text", "success");
+    }
+
+    const handleCapClick = () => {
+        const words = text.split(" ");
+        for(let i=0; i<words.length; i++) {
+            words[i] = words[i][0].toUpperCase() + words[i].slice(1).toLowerCase();
+        }
+        let newText = words.join(" ");
+        setText(newText);
+        props.showAlert("Capitalized text", "success");
+    }
+
+    const handleExtraClick = () => {
+      const regex = / +/g;
+      let newText = text.replace(regex, " ");
+      setText(newText);
+      props.showAlert("Removed extra spaces", "success");
+    }
+
+    const handleOnChange = (event) => {
+        // console.log("text was changed");
+        setText(event.target.value)
+    }
+  const [text, setText] = useState(""); //state, also using array destructuring
+//   text = "shsidi";//Not allowed
+    //  setText("new text"); //correct way
+
+  return (
+      <>
+    <div className=" container my-3" style={{color: props.mode==="light"?"black":"white"}}>
+      <h1>{props.heading}</h1>
+      <textarea
+        className="form-control"
+        id="exampleFormControlTextarea1"
+        rows="8"
+        value={text}
+        onChange={handleOnChange}
+        style={{backgroundColor: props.mode==="light"?"white":"rgb(154 162 170)", color: props.mode==="light"?"black":"white"}}
+      ></textarea>
+      <button className="btn btn-primary my-2 mx-1" onClick={handleUpClick}>Change to uppercase</button>
+      <button className="btn btn-primary my-2 mx-1" onClick={handleLowClick}>Change to lowercase</button>
+      <button className="btn btn-primary my-2 mx-1" onClick={handleClearClick}>Clear</button>
+      <button className="btn btn-primary my-2 mx-1" onClick={handleCapClick}>Capitalize text</button>
+      <button className="btn btn-primary my-2 mx-1" onClick={handleExtraClick}>Remove extra spaces</button>
+
+    </div>
+    <div className="container my-3" style={{color: props.mode==="light"?"black":"white"}}>
+        <h2>Your text summary</h2>
+        <p><b>{text.split(" ").filter((element) =>  element!=="").length}</b> words and <b>{text.length}</b> characters</p>
+        <p><b>{0.008*text.split(" ").filter((element) =>  element!=="").length}</b> minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
+  );
+}
